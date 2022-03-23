d4c0d9695644cb28c143b93b70553800384b7f21
Sesil <s.tasim@student.fontys.nl>
2022-03-23 17:24:02 +0100

    FE SkatePage done

--- frontend-files/skatrixx-app/src/components/SkatePage.js
+++ frontend-files/skatrixx-app/src/components/SkatePage.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import Loby from "./Loby"
+import React from "react";
+
+import "../stylesheets/SkatePage.css";
 
 function SkatePage() {
   return (
-    <></>
-    // <div>SkatePage</div>
-  )
+    <div>
+      <h2 id="TrickName">Ollie</h2>
+      <div id="VideoDiv">
+        <iframe
+          id="videoFrame"
+          src="https://www.youtube.com/embed/E7wJTI-1dvQ"
+          frameborder="0"
+          allow="autoplay; encrypted-media"
+          allowfullscreen
+          title="video"
+        />{" "}
+      </div>
+
+      <h5>Description:</h5>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+
+      <button type="button" onclick="">
+        START
+      </button>
+    </div>
+  );
 }
 
-export default SkatePage
+export default SkatePage;
