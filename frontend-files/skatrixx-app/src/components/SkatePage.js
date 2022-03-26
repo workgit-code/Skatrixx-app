4cdaaf706b29081d7679fa9fd6e4b03ddb74af30
Takev,Antonio A.M <a.takev@student.fontys.nl>
2022-03-26 23:09:39 +0100

    Connect FE and BE for skateData - Service function for handling api requests to API for Skate Data - api_client for creating axios instance placed in separate file to avoid code repetition - axios calling functions from service file using async-await method - Error handling + Loading functionality added - Map for the whole list of skate data to present the data

--- frontend-files/skatrixx-app/src/components/SkatePage.js
+++ frontend-files/skatrixx-app/src/components/SkatePage.js
@@ -1,8 +1,53 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
+import skateDataService from '../services/skateDataService'
+import axios from 'axios'
 
 function SkatePage() {
+  const [skateData, setSkateData] = useState([]) // skateDate useState
+  const [loading, setLoading] = useState(true) // Loading
+  const [error, setError] = useState(""); // Error
+
+  //  update Data on component reload
+  useEffect(() => {
+    retrieveSkateData()
+  }, [])
+  
+  // Get skate data (async await axiox data retrieving)
+  const retrieveSkateData = async () => {
+    setLoading(true)
+    try {
+      const res = await skateDataService.getAll()
+      setSkateData(res.data)
+      // console.log(res.data)
+    } 
+    catch (err) {
+      // console.log(err.message)
+      setError("Error occurs while retrieving skate data!")
+    }
+    setLoading(false)
+  }
+
   return (
+    <div>
     <div>SkatePage</div>
+    {loading && <div>Loading...</div>}
+    {!loading && (
+      !error ? (
+        <div>
+        {skateData.map(item => 
+        (
+          <span key={item._id}>{item.airtime}</span>
+        )
+        )}
+      </div>
+      )
+      :
+      (
+        <div>{error}</div>
+      )
+      
+    )}
+    </div>
   )
 }
 
