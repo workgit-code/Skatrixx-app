88cc418da9a5ddd19eb9870e9b94764e88de33ac
Churkina,Yoana Y.K <82328697+Yoana01@users.noreply.github.com>
2022-03-30 10:10:46 +0200

    set the base of the connection of the pages

--- express-server/services/levelService.js
+++ express-server/services/levelService.js
@@ -4,7 +4,7 @@ function levelUp(input) {
     let sum = 0
     let tricks = trick.find();
     for (let i = 0; i <= tricks.length; i++) {
-        let current = tricks[i]
+        let current = tricks[i]  
         if(current === tricks.name) {sum += tricks.xp}
             console.log("Congrats")
         }
--- frontend-files/skatrixx-app/src/components/LevelMenu.js
+++ frontend-files/skatrixx-app/src/components/LevelMenu.js
@@ -1,26 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import LevelList from './LevelList'
 import "../stylesheets/LevelMenu.css"
 
 
 function LevelMenu(props) {
     // Use state (hooks)
-
+    const [difficultyOpened, setDificultyOpened] = useState('');
     // Use effect - load levels
 
     // Functions - levels logic
   
+    const handleDifficultyChange = (dif) => {
+        setDificultyOpened(dif)
+    }
+
+    if(difficultyOpened === '') {
     return ( <div className='level-menu'>
         <p id='back' onClick={() => {props.back('Menu')}}>&lt;</p>
-            {/* <h3>Beginner</h3>
-            <LevelList/>
-            <h3>Intermediate</h3>
-            <LevelList/>
-            <h3>Master</h3>
-            <LevelList/> */}
-            <LevelList difficulty = {'master'} alley={"Beginner's Alley"}/>
+        <h3 onClick={() => {handleDifficultyChange('rookie')}}>Rookie Park</h3>
+        <h3 onClick={() => {handleDifficultyChange('amateur')}}>amateur Park</h3>
+        <h3 onClick={() => {handleDifficultyChange('pro')}}>pro Park</h3>
         </div>
     )
+    }
+    else {
+            return (<LevelList difficulty = {'beginner'} alley={"Beginner's Alley"} back={}/>)
+    }
 }
 
 export default LevelMenu
