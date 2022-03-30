139521337710ff351c56504696e2ac6077c69afa
Churkina,Yoana Y.K <82328697+Yoana01@users.noreply.github.com>
2022-03-30 16:20:13 +0200

    connected all the pages in the solo part

--- frontend-files/skatrixx-app/src/components/TrickPage.js
+++ frontend-files/skatrixx-app/src/components/TrickPage.js
@@ -1,32 +1,61 @@
-import {React, useState, useEffect} from 'react'
-import "../stylesheets/TrickPage.css"
+import { React, useState, useEffect } from "react";
+import "../stylesheets/TrickPage.css";
+import { useTimer } from "react-timer-hook";
+import Statistic from "./Statistic";
 
 function TrickPage(props) {
+  const expiryTimestamp = new Date();
+  expiryTimestamp.setSeconds(expiryTimestamp.getSeconds() + 60); // 1 minute timer
+  const { seconds, reset, start, pause } = useTimer({
+    expiryTimestamp,
+    autoStart: false,
+    onExpire: () => console.warn("onExpire called"),
+  });
+  const [isStarted, setIsStarted] = useState(false);
+  const [timerFinished, setTimerFinished] = useState(false)
+
+  const toggleStart = () => {
+
+    if (!isStarted) {
+      start();
+    } else {
+      pause();
+      setTimerFinished(true)
+
+    }
+
+    setIsStarted(!isStarted);
+  };
 
   return (
+   !timerFinished ? (
     <div>
-      <h2 id="TrickName">{props.trick.name}</h2>
-      <div id="VideoDiv">
-        <iframe
-          id="videoFrame"
-          src={props.trick.videoLink}
-          frameborder="0"
-          allow="autoplay; encrypted-media"
-          allowfullscreen
-          title="video"
-        />{" "}
-      </div>
-
-      <h5>Description:</h5>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </p>
-
-      <button type="button" onclick="">
-        START
-      </button>
+    <h2 id="TrickName">{props.trick.name}</h2>
+    <div id="VideoDiv">
+      <iframe
+        id="videoFrame"
+        src={props.trick.videoLink}
+        frameborder="0"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+        title="video"
+      />{" "}
     </div>
+
+    <h5>Description:</h5>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+
+    <button type="button" onClick={toggleStart}>
+      {!isStarted ? "Start" : "Done"}
+    </button>
+    <div id="sec">{seconds}</div>
+  </div>
+   ): (
+   <Statistic/>
+   )
   );
 }
 
