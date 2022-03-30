4b3d07c15efe79f6fc5606ab4e38d6c6f95791f6
Takev,Antonio A.M <a.takev@student.fontys.nl>
2022-03-30 19:22:28 +0200

    CSS fix levelList

--- frontend-files/skatrixx-app/src/components/LevelContainer.js
+++ frontend-files/skatrixx-app/src/components/LevelContainer.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import "../stylesheets/LevelContainer.css"
-import PlayIcon from "../images/Play.svg"
+import PlayIcon from "../images/Play.png"
 
 function LevelContainer(props) {  
  
@@ -11,12 +11,12 @@ function LevelContainer(props) {
 
   return (
       
-      <div id='trickList'> 
-      <div className='trick'>
+      <div id='trick'>
         <p id="trickName">{props.trick.name}</p> 
-        <p id="trickXp">{props.trick.xp}</p>
-        <img onClick={openTrick} src={PlayIcon} id="play"/> 
-      </div>
+        <div className='trick-right'>
+        <p id="trickXp">{props.trick.xp}xp</p>
+        <i className="material-icons play-trick-arrow" onClick={openTrick}>keyboard_arrow_right</i>
+        </div>
       </div>
       
   )
