8009f81f34a264a87500e945f939c1eb13e84daf
Sesil <s.tasim@student.fontys.nl>
2022-03-30 12:22:19 +0200

    FE LevelMenu modify

--- frontend-files/skatrixx-app/src/components/LevelMenu.js
+++ frontend-files/skatrixx-app/src/components/LevelMenu.js
@@ -1,41 +1,77 @@
-import React, {useState} from 'react'
-import LevelList from './LevelList'
-import "../stylesheets/LevelMenu.css"
-import IntermediateRamp from "../images/IntermediateRamp.jpeg"
-
+import React, { useState } from "react";
+import LevelList from "./LevelList";
+import "../stylesheets/LevelMenu.css";
+import RookieRamp from "../images/RookieRamp.png";
+import IntermediateRamp from "../images/IntermediateRamp.png";
+import ProsRamp from "../images/ProsRamp.png";
 
 function LevelMenu(props) {
-    // Use state (hooks)
-    const [difficultyOpened, setDificultyOpened] = useState('');
-    // Use effect - load levels
+  // Use state (hooks)
+  const [difficultyOpened, setDificultyOpened] = useState("");
+  // Use effect - load levels
 
-    // Functions - levels logic
-  
-    const handleDifficultyChange = (dif) => {
-        setDificultyOpened(dif)
-    }
+  // Functions - levels logic
 
-    if(difficultyOpened === '') {
-    return ( <div className='level-menu'>
-        <p id='back' onClick={() => {props.back('Menu')}}>&lt;</p>
-        <div class="beginner">
-        <img src="../images/RookieRamp.jpeg"/>
-        <h3 onClick={() => {handleDifficultyChange('rookie')}}>Rookie Park</h3>
+  const handleDifficultyChange = (dif) => {
+    setDificultyOpened(dif);
+  };
+
+  if (difficultyOpened === "") {
+    return (
+      <div className="level-menu">
+        <p
+          id="back"
+          onClick={() => {
+            props.back("Menu");
+          }}
+        >
+          &lt;
+        </p>
+        <div className="beginner">
+          <img src={RookieRamp} id="RookieRamp" />
+          <h3>
+            Rookie's Park
+          </h3>
+          <h5>10 Tricks</h5>
+          <button type="button" onClick={() => {
+              handleDifficultyChange("rookie");
+            }} id="RookieButton">
+        Go →
+      </button>
         </div>
-        <div class="intermediate">
-        <img src={IntermediateRamp}/>
-        <h3 onClick={() => {handleDifficultyChange('amateur')}}>amateur Park</h3>
+        <div className="intermediate">
+          <img src={IntermediateRamp} id="IntermediateRamp" />
+          <h3>
+            Amateur's Park
+          </h3>
+          <h5>20 Tricks</h5>
+          <button type="button"  onClick={() => {
+              handleDifficultyChange("amateur");
+            }} id="IntermediateButton">
+        Go →
+      </button>
         </div>
-        <div class="master">
-        <img src="../images/ProsRamp.jpeg"/>
-        <h3 onClick={() => {handleDifficultyChange('pro')}}>pro Park</h3>
+        <div className="master">
+          <img src={ProsRamp} id="ProsRamp" />
+          <h3
+            onClick={() => {
+              handleDifficultyChange("pro");
+            }}
+          >
+            Pro's Park
+          </h3>
+          <h5>25 Tricks</h5>
+          <button type="button" onclick="" id="ProsButton">
+        Go →
+      </button>
         </div>
-        </div>
-    )
-    }
-    else {
-            return (<LevelList difficulty = {'beginner'} alley={"Beginner's Alley"}/>)
-    }
+      </div>
+    );
+  } else if(dif==="rookie"){
+    return <LevelList difficulty={"beginner"} alley={"Beginner's Alley"} />;
+  } else if (dif==="amateur"){
+    return <LevelList difficulty={"intermidiate"} alley={"Bkjj's Alley"} />;
+  }
 }
 
-export default LevelMenu
+export default LevelMenu;
