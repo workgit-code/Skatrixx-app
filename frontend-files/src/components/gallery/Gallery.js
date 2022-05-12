39680f111ffb5832fd371313fc941424aaf4f9c3
Churkina,Yoana Y.K <82328697+Yoana01@users.noreply.github.com>
2022-05-12 18:46:01 +0200

    upload image, fix css

--- frontend-files/src/components/gallery/Gallery.js
+++ frontend-files/src/components/gallery/Gallery.js
@@ -1,5 +1,8 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import "../../stylesheets/gallery/Gallery.css"
+import {storage} from '../../services/firebaseService'
+import {ref, uploadBytes, listAll, getDownloadURL} from "firebase/storage" //reference where in our bucket is the image,listing all the files
+import {v4} from 'uuid' //for randomizing letters
 import image1 from "../../images/skateboards/1.jpg"
 import image2 from "../../images/skateboards/2.jpg"
 import image3 from "../../images/skateboards/3.jpg"
@@ -7,50 +10,98 @@ import image4 from "../../images/skateboards/4.jpg"
 import imageAdd from "../../images/add new image.png"
 import CameraComponent from './CameraComponent' 
 import UploadImage from './UploadImage' 
-import Modal from './Modal' 
-import { UnpackDepthRGBAShader } from 'three-stdlib'
+import Modal from './Modal'
+
+
 function Gallery() {
+  // --- useStates or ref
+
+  // View certain image
   const [viewedImage, setViewedImage] = useState(undefined);
-  const images = [
-    image1,
-    image2,
-    image3,
-    image4,
-    image1,
-    image2,
-    image3,
-    image1,
-    image2,
-    image3,
-    image4
-  ]
+  const [imageUpload, setImageUpload] = useState(null)
+  const [imageUrls, setImageUrls] = useState([]);
+  const [outputImg, setOutputImg] = useState(null)
+  const [isOpen, setIsOpen] = useState(false);
+  const [galleryMode, setgalleryMode] = useState('Gallery');
+  const imageListRef = ref(storage, "images/")
   
+  // const images = [
+  //   image1,
+  //   image2,
+  //   image3,
+  //   image4,
+  //   image1,
+  //   image2,
+  //   image3,
+  //   image1,
+  //   image2,
+  //   image3,
+  //   image4
+  // ]
+  
+  useEffect(() => {
+    listAll(imageListRef).then((response) => {
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageUrls((prev) => [...prev, url]);
+        });
+      });
+    });
+  }, [])
+
+  // --- Methods
+  const onImageChosen = (event) => {
+    setImageUpload(event.target.files[0])
+    if (event.target.files && event.target.files[0]) {
+      setOutputImg(URL.createObjectURL(event.target.files[0]))
+    }
+  }
 
   const handleOpenImage = (img) => {
     setViewedImage(img);
   }
-  const [isOpen, setIsOpen] = useState(false);
  
   const togglePopup = () => {
     setIsOpen(!isOpen);
   }
-  const [galleryMode, setgalleryMode] = useState('Gallery');
+  const uploadFile = () => {
+    if (imageUpload == null) return;
+    //functions from firebase
+    ////the name and the path where we store it as we are making folder as well as to make it unique
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`) 
+    //where to be upload and the image itself 
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageUrls((prev) => [...prev, url])
+      })
+    })
+    togglePopup()
+  }
+  
+  
   const handleChange = () => {
     if(galleryMode === 'Gallery') {
       return (
         <div className='gallery'>
-          {/* <CameraComponent/> */}
-          <img src={imageAdd} id="addimg" onClick={togglePopup}></img>
         {isOpen && <Modal
           content={<>
-           <button className="skateBtn" id="upload" onClick={() => {setgalleryMode("Upload")}}>Upload Image</button>
-           <button className="skateBtn" id="camera" onClick={() => {setgalleryMode("Camera")}}>Camera</button>
+          <div id="outputImgContainer">
+            {outputImg !== "" ? (<img src={outputImg} id="outputImg" alt=""/>) : (<></>)}
+            <button onClick={uploadFile}> Upload Image</button>
+          </div>
           </>}
           handleClose={togglePopup}
           />}
-          {images.map(image => (
+          <div className='gallery-container'>
+                <label htmlFor="image_input" >
+                  <img src={imageAdd} alt="upload-button" id="uploading" onClick={togglePopup}/>
+                </label>
+                <input type="file" id="image_input" name="file"  onChange={onImageChosen}></input>
+              {/* <img src={imageAdd} id="addimg" ></img> */}
+          </div>
+          {imageUrls.map(image => (
             <div className='gallery-container' onClick={() => {handleOpenImage(image)}}>
-              <img src={image} alt=''/>
+              <img src={image}  className="galleryImage" alt=''/>
             </div>
           ))}
           {viewedImage !== undefined ? 
@@ -61,25 +112,11 @@ function Gallery() {
         </div>
       )
     }
-    else if(galleryMode === 'Upload'){
-      return(
-        <div className='friends-tab'>
-          <UploadImage/> 
-        </div>
-    )
-    } 
-    else if(galleryMode === 'Camera'){
-      return(
-        <div className='friends-tab'>
-            <CameraComponent/>
-        </div>
-    )
-    }    
- 
 }
 return(
   handleChange()   
 )
 }
+
 
 export default Gallery
--- frontend-files/src/components/gallery/UploadImage.js
+++ frontend-files/src/components/gallery/UploadImage.js
@@ -2,18 +2,20 @@ import {useState, useEffect} from "react";
 import {storage} from '../../services/firebaseService'
 import {ref, uploadBytes, listAll, getDownloadURL} from "firebase/storage" //reference where in our bucket is the image,listing all the files
 import {v4} from 'uuid' //for randomizing letters
+import imageAdd from "../../images/add new image.png"
 // import { set } from 'mongoose';
 // something with rules only people logged in
 // register the app in project settings 
 
-function App() {
+function UploadImage() {
   const [imageUpload, setImageUpload] = useState(null)
   const [imageUrls, setImageUrls] = useState([]);
+  const [outputImg, setOutputImg] = useState(null)
 
   const imageListRef = ref(storage, "images/")
 
   const uploadFile = () => {
-    if (imageUpload == null) return;
+    if (imageUpload == null)      return;
     //functions from firebase
     ////the name and the path where we store it as we are making folder as well as to make it unique
     const imageRef = ref(storage, `images/${imageUpload.name + v4()}`) 
@@ -25,6 +27,13 @@ function App() {
       
     }
  )}
+ const onImageChosen = (event) => {
+  setImageUpload(event.target.files[0])
+  if (event.target.files && event.target.files[0]) {
+    setOutputImg(URL.createObjectURL(event.target.files[0]))
+  }
+ }
+
  useEffect(() => {
   listAll(imageListRef).then((response) => {
     response.items.forEach((item) => {
@@ -34,22 +43,20 @@ function App() {
     });
   });
 }, []);
-  return (     
-         <div className="App">
-             {/* image upload will take the value of the input and to access the file and can access multiple and put them in array */}
-         <input
-           type="file"
-           onChange={(event) => {
-             setImageUpload(event.target.files[0]);
-           }}
-         />
+
+  return(
+    <div>
+        {/* image upload will take the value of the input and to access the file and can access multiple and put them in array */}     
+             {outputImg !== "" ? (<img src={outputImg} alt=""/>) : (<></>)}
          <button onClick={uploadFile}> Upload Image</button>
+         <div className="displayImages">
          {imageUrls.map((url) => {
-           return <img src={url} />;
+           return <img src={url} className="galleryImage" />;
          })}
-</div>
-  );
+         </div>
+    </div>
+  )
 }
 
-export default App;
+export default UploadImage;
 
